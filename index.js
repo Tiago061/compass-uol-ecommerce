d156f45eb94b76c5f3d14aadcdcb949f173807e2
Tiago Henrique <thenriqueleite277@gmai.com>
2025-02-17 09:51:19 -0300

    feat: add feature input newsletter

---
+++ index.js
@@ -0,0 +1,28 @@
+
+const emails = []
+
+//função de validação de email.
+function validateEmail(email){
+    const pattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    return pattern.test(email);
+}
+
+//função de verificação de email.
+function verifyEmail(){
+    const emailInput = document.getElementById("input-newsletter").value.trim()
+    console.log("Entered email:", emailInput); // Debugging
+
+
+    if(!validateEmail(emailInput)){
+        alert("Please enter a valid email address.")
+        return;
+    }
+
+    if (emails.includes(emailInput)) {
+        alert("This email is already registered.");
+        return;
+    }
+
+    emails.push(emailInput);
+    alert("Email successfully subscribed!");
+}
